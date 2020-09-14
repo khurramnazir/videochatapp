95dd3dbb9500bd3c788743f976556b4277756c9b
Northcoders <northcoders@Northcoderss-MacBook-Pro.local>
2020-09-14 09:41:11 +0100

    Basic functionality of pairUp function implemented and tested

--- utils/index.js
+++ utils/index.js
@@ -1,1 +1,16 @@
-exports.pairUp = () => {};
+exports.pairUp = (allMembers) => {
+  const pairs = [];
+  const allMembersCopy = [...allMembers];
+  const evenIndices = allMembersCopy.filter((member, index) => {
+    return index % 2 === 0;
+  });
+  const oddIndices = allMembersCopy.filter((member, index) => {
+    return index % 2 !== 0;
+  });
+
+  for (let i = 0; i < allMembersCopy.length * 0.5; i++) {
+    pairs.push([evenIndices[i], oddIndices[i]]);
+  }
+
+  return pairs;
+};
--- utils/index.test.js
+++ utils/index.test.js
@@ -1,5 +1,8 @@
 const { pairUp } = require("./index");
 
 describe("pairUp()", () => {
-  test("", () => {});
+  test("accepts an array of unique id's and returns an array of sub-arrays containing unique pairs", () => {
+    const test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    expect(pairUp(test)).toHaveLength(5);
+  });
 });
