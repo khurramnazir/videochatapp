4da5e36e28fe5f1bf7830db9b7bb758d1640d8f4
khurramnazir <khurram.nazir01@gmail.com>
2020-09-14 12:52:20 +0100

    built routes

--- video_chat_app/src/App.js
+++ video_chat_app/src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import './App.css';
-import Header from './Components/Header'
+import React from "react";
+import "./App.css";
+import Header from "./Components/Header";
 import { Router } from "@reach/router";
-import RoomCreator from './Components/RoomCreator'
-import Lobby from './Components/Lobby'
-import Login from './Components/Login'
-import ErrorPage from './Components/ErrorPage'
-
+import RoomCreator from "./Components/RoomCreator";
+import Lobby from "./Components/Lobby";
+import Login from "./Components/Login";
+import ErrorPage from "./Components/ErrorPage";
 
 const App = () => {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Router>
-          <RoomCreator path="/"/>
-          <Lobby path="/:roomLobby"/>
-          {/* <Login path="/login/:roomLobby"/>
-          <ErrorPage default status={404} msg={"Path not found"} /> */}
-        </Router>
-    </ div>
+        <RoomCreator path="/" />
+        <Lobby path="/:roomLobby" />
+        <Login path="/login/:roomLobby" />
+        {/* <ErrorPage default status={404} msg={"Path not found"} /> */}
+      </Router>
+    </div>
   );
 };
 
--- video_chat_app/src/Components/RoomCreator.jsx
+++ video_chat_app/src/Components/RoomCreator.jsx
@@ -1,23 +1,46 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from "react";
 import { navigate } from "@reach/router";
 
-
 const RoomCreator = () => {
-    const [name, setName] = useState("");
-    const [roomName, setRoomName] = useState("");
+  const [name, setName] = useState("");
+  const [roomName, setRoomName] = useState("");
 
-    console.log(name, roomName)
+  const handleSubmit = (submitEvent) => {
+    submitEvent.preventDefault();
+    navigate(`/${roomName}`, {
+      state: { name, type: "admin" },
+    });
+    // const socket = io.connect("http://localhost:5000");
+  };
 
-    return (
-        <>
-           <p>Welcome! Please create a room by filling in the details below...</p> 
-           <form onsubmit={navigate(`/${roomName}`)}>
-               <input type="text" id="name" name="name" placeholder="Your Name" onChange={(e)=>{setName(e.target.value)}} ></input>
-               <input type="text" id="roomName" name="roomName" placeholder="Room Name" onChange={(e)=>{setRoomName(e.target.value)}} ></input>
-               <button type="submit" >Create Room</button>
-           </form>
-        </>
-    );
+  return (
+    <>
+      <p>Welcome! Please create a room by filling in the details below...</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Your Name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          required
+        ></input>
+        <input
+          type="text"
+          id="roomName"
+          name="roomName"
+          placeholder="Room Name"
+          onChange={(e) => {
+            setRoomName(e.target.value);
+          }}
+          required
+        ></input>
+        <button type="submit">Create Room</button>
+      </form>
+    </>
+  );
 };
 
 export default RoomCreator;
